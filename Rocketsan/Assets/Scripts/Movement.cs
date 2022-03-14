99b99ba55d1102cbf1aa17ecd9cca43f32683e5f
Cevdet Eren <82716289+cevdetozbozkurt@users.noreply.github.com>
2022-03-14 19:51:18 +0300

    boost particle system and scripting

--- Rocketsan/Assets/Scripts/Movement.cs
+++ Rocketsan/Assets/Scripts/Movement.cs
@@ -11,11 +11,18 @@ public class Movement : MonoBehaviour
     [SerializeField] AudioClip mainEngine;
 
     AudioSource rocket_audio;
-    
+    ParticleSystem boost;
+    ParticleSystem rightBoost;
+    ParticleSystem leftBoost;
+
+  
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rocket_audio = GetComponent<AudioSource>();
+        boost = GameObject.Find("Rocket Jet Particles").GetComponent<ParticleSystem>();
+        leftBoost = GameObject.FindGameObjectWithTag("Left boost").GetComponent<ParticleSystem>();
+        rightBoost = GameObject.FindGameObjectWithTag("Right boost").GetComponent<ParticleSystem>();
     }
 
     void Update()
@@ -32,11 +39,17 @@ public class Movement : MonoBehaviour
             if(!rocket_audio.isPlaying)
             {
                 rocket_audio.PlayOneShot(mainEngine);
+                
+            }
+            if (!boost.isPlaying)
+            {
+                boost.Play();
             }
         }
         else
         {
             rocket_audio.Stop();
+            boost.Stop();
         }
     }
 
@@ -45,10 +58,23 @@ public class Movement : MonoBehaviour
         if(Input.GetKey(KeyCode.A))
         {
             ApplyRotation(rotationThrust);
+            if(!leftBoost.isPlaying)
+            {
+                leftBoost.Play();
+            }
         }
         else if(Input.GetKey(KeyCode.D))
         {
-            ApplyRotation(-rotationThrust);
+            ApplyRotation(-rotationThrust); 
+            if (!rightBoost.isPlaying)
+            {
+                rightBoost.Play();
+            }
+        }
+        else
+        {
+            rightBoost.Stop();
+            leftBoost.Stop();
         }
     }
 
