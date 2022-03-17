8ab7fe01baa36ef1d327bf21c3e0b96e0ed90e62
Cevdet Eren <82716289+cevdetozbozkurt@users.noreply.github.com>
2022-03-17 15:31:17 +0300

    tp denemesi v2
    transform değiştirmeye çalıştım ama olmadı.

--- Rocketsan/Assets/Scripts/PortalControl.cs
+++ Rocketsan/Assets/Scripts/PortalControl.cs
@@ -7,20 +7,42 @@ public class PortalControl : MonoBehaviour
 
     ParticleSystem PortalEnter;
     ParticleSystem PortalExit;
+    [SerializeField] GameObject rocket;
+    
+    Vector3 PortalEnterTransform;
+    Vector3 PortalExitTransform;
+    Vector3 RocketTransform;
 
     void Start()
     {
-        PortalEnter = GameObject.FindGameObjectWithTag("Portal Enter").GetComponent<ParticleSystem>();    
+        PortalEnter = GameObject.FindGameObjectWithTag("Portal Enter").GetComponent<ParticleSystem>();
+        PortalExit = GameObject.FindGameObjectWithTag("Portal Exit").GetComponent<ParticleSystem>();
+        PortalEnterTransform = PortalEnter.transform.position;
+        PortalExitTransform = PortalExit.transform.position;
+        Debug.Log("Rocket Transform = " + RocketTransform);
+        Debug.Log("Portal Enter Transform ="+PortalEnterTransform);
+        Debug.Log("Portal Exit Transform = "+PortalExitTransform);
     }
 
     void Update() 
     {
         
+        RocketTransform = rocket.transform.position;
+        Debug.Log("New Rocket Transform"+RocketTransform);
     }
 
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Portal Enter"){
+            RocketTransform = PortalExitTransform;
+            Debug.Log("After tp rocket transform = "+RocketTransform);
+        }
+    }
     void EnterPortal()
     {
-
+        if(RocketTransform == PortalEnterTransform)
+        {
+            RocketTransform = PortalExitTransform;
+        }
     }
 
 }
